19ca3895e30dd582a4529aacd62a3ebdd4ec8b12
Philip Almueti <palmueti@gmail.com>
2024-12-12 07:27:35 -0800

    Add freerdp to attack box and ntfs toolsuites to all boxes

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -18,8 +18,15 @@
         useGlobalPkgs = true;
     };
 
+    # Enable ntfs mounting
+    services.devmon.enable = true;
+    services.gvfs.enable = true;
+    services.udisks2.enable = true;
+    
     environment.systemPackages = with pkgs; [
         wireguard-go
+        ntfs3g
+        parted
         openconnect
     ];
 }
