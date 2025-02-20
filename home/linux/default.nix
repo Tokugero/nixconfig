9028d888eb54c5a8f46498a4a5e4829aafdf8949
Philip Almueti <palmueti@gmail.com>
2025-02-19 21:58:10 -0800

    Refactor a little

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -31,8 +31,6 @@
 
     powerManagement.enable = false;
 
-    programs.firefox.enable = true;
-
     environment.systemPackages = with pkgs; [
         brave
         wireguard-go
@@ -40,26 +38,28 @@
         parted
         openconnect
     ];
-
-    programs.nix-ld.enable = true;
     
     virtualisation.docker = {
         enable = true;
         enableOnBoot = true;
     };
 
-    programs.zsh.interactiveShellInit = ''
-        nixupdate() {
-            OLDDIR=$(pwd);
-            cd ~/.nix;
-            # If $1 is not set, then use hostname
-            if [ -z "$1" ]; then
-                echo "No hostname provided, using $(hostname)";
-                sudo nixos-rebuild switch --impure --upgrade --flake .#$(hostname);
-            else
-                sudo nixos-rebuild switch --impure --upgrade --flake .#$1
-            fi
-            cd $OLDDIR;
-        };
-    '';
+    programs = {
+        nix-ld.enable = true;
+        firefox.enable = true;
+        zsh.interactiveShellInit = ''
+            nixupdate() {
+                OLDDIR=$(pwd);
+                cd ~/.nix;
+                # If $1 is not set, then use hostname
+                if [ -z "$1" ]; then
+                    echo "No hostname provided, using $(hostname)";
+                    sudo nixos-rebuild switch --impure --upgrade --flake .#$(hostname);
+                else
+                    sudo nixos-rebuild switch --impure --upgrade --flake .#$1
+                fi
+                cd $OLDDIR;
+            };
+        '';
+    };
 }
