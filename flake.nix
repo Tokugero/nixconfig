f5fb02f2fe52748ca72f91a915cdfc9366decab0
Philip Almueti <palmueti@gmail.com>
2025-02-21 11:25:08 -0800

    Clean some formatting

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
